dd69d40ff5a223c212a266316211a3f3afef51b4
raiyen <raiyen@slightech.com>
2018-12-21 13:19:46 +0800

    Initial the vue project

---
+++ personal/src/components/Skill.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="skill" id="skill"></div>
+</template>
+
+<script>
+// 引入基本模板
+let echarts = require("echarts/lib/echarts");
+export default {
+  name: "skill",
+   data() {
+    return {};
+  },
+  mounted() {
+    this.drawECharts();
+  },
+  methods: {
+    drawECharts() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById("skill"));
+      myChart.setOption({
+        // 标题
+        title: {
+          // 标题文本
+          text: "技能分布图",
+          // 标题样式
+          textStyle: {
+            color: "#333"
+          },
+          // 标题位置
+          x: "center"
+        },
+        // 移动显示
+        tooltip: {
+          trigger: "item",
+          // 显示文字样式
+          formatter: "{a} <br/>{b} : {d}%"
+        },
+        // 注记
+        legend: {
+          x: "center",
+          y: "bottom",
+          textStyle: {
+            color: "#333"
+          },
+          data: [ "HTML5", "CSS3", "JavaScript", "jQuery", "Scss/Sass","Vue", "微信小程序", "其他" ]
+        },
+        // 注记显示手柄
+        calculable: true,
+        // 图形系列
+        series: [
+          {
+            name: "技能分布",
+            type: "pie",
+            radius: [30, 110],
+            roseType: "area",
+            data: [
+              { value: 15, name: "HTML5" },
+              { value: 15, name: "CSS3" },
+              { value: 10, name: "JavaScript" },
+              { value: 20, name: "jQuery" },
+              { value: 15, name: "Scss/Sass" },
+              { value: 20, name: "Vue" },
+              { value: 20, name: "微信小程序" },
+              { value: 15, name: "其他" }
+            ]
+          }
+        ],
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.skill {
+  width: 100%;
+  height: 500px;
+}
+</style>
---
+++ personal/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 引入样式重置
+import '../static/css/reset.css'
+
+//引入echarts(全局引用 建议使用按需引用)
+// import echarts from 'echarts'
+// Vue.prototype.$echarts = echarts
+
+// 引入及使用 ElementUI 按需引入 Row 与 Col(借助 babel-plugin-component，我们可以只引入需要的组件，以达到减小项目体积的目的。)
+import {Row, Col} from 'element-ui';
+Vue.use(Row).use(Col);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
